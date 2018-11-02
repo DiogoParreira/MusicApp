10e1f135986d22613788fbe1ccd1d2ee59b9f261
DiogoParreira <diogomparreira@gmail.com>
2018-11-02 19:37:03 +0000

    Added swipe down to refresh

--- app/src/main/java/net/diogoparreira/musicapp/MainActivity.kt
+++ app/src/main/java/net/diogoparreira/musicapp/MainActivity.kt
@@ -2,6 +2,7 @@ package net.diogoparreira.musicapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: MusicRecyclerViewAdapter
     private val musicList = mutableListOf<MusicEntryData>()
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +26,23 @@ class MainActivity : AppCompatActivity() {
         recyclerView.itemAnimator = DefaultItemAnimator()
         recyclerView.adapter = adapter
 
+        swipeRefreshLayout = findViewById(R.id.swiperefresh)
 
+        swipeRefreshLayout.setOnRefreshListener { updateMusicList() }
+
+        updateMusicList()
+
+    }
+
+    private fun updateMusicList() {
+        swipeRefreshLayout.isRefreshing = true
         MusicApplication.instance.getMusicList("Portugal", object: MusicListCallback {
             override fun onReceiveList(musicList: List<MusicEntryData>) {
                 adapter.musicList = musicList
                 adapter.notifyDataSetChanged()
+                swipeRefreshLayout.isRefreshing = false
             }
 
         })
-
     }
 }
